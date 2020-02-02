9ba9d96b9f8f888c75e764b52c2d9b3fe03b50a1
islamhanafi94 <islamhanafi1994@gmail.com>
2020-02-02 21:50:02 +0200

    adding delete function

--- bashdb.sh
+++ bashdb.sh
@@ -16,9 +16,9 @@ RunDBMS () {
   read ch
   case $ch in
     1)  listDB ;; #show all function
-    2)  echo 1 ;;
+    2)  createDB ;;
     3)  echo 1 ;;
-    4)  echo 1 ;;
+    4)  deleteDB ;;
     5)  echo 2 ;;
     6)  exit;;
     *) echo " Invalid input " ; 
@@ -32,14 +32,20 @@ listDB (){
     clear;
     echo ">>>>>Databases list<<<<<"
     ls -1 ./DBs; # how to display list in new line
-    echo "------------------------"
+    backToMainMenu
+
+}
+
+backToMainMenu(){
     PS3="What Do you want to do ? : ";
+    echo "------------------------"
     select varName in "Back to menu" "Exit"
     do
 	case $varName in
 		"Back to menu")
 			clear;
             RunDBMS;
+            break;
             ;;
 		"Exit")
 			clear;
@@ -50,10 +56,32 @@ listDB (){
             exit;
             ;;
 	esac			
-
 done
-
-
 }
-RunDBMS
+createDB (){
+    clear;
+    echo ">>>>> Create new Database <<<<<"
+    echo -n "Enter Database name : "
+    read DBname
+    # we should check if the name is already taken
+    mkdir ./DBs/$DBname;
+    backToMainMenu;
+    
+}
 
+deleteDB() {
+    clear;
+    echo ">>>>>Databases list<<<<<"
+    echo -n "Enter Database name : "
+    read droppedDB
+    rm -r DBs/$droppedDB
+    if [ $? -eq 0 ]; 
+    then 
+        echo "${droppedDB} Database Removed Successfuly!"
+    else 
+        echo "Failed!" 
+    fi
+    backToMainMenu
+}
+
+RunDBMS
